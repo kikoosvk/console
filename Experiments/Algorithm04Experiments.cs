d8a68ef36bc2fddedb17aa38e69dac13db7aa76f
Kristian Chudjak <kristian.chudjak@siemens-healthineers.com>
2019-10-04 13:59:21 +0200

    asfd

--- Experiments/Algorithm04Experiments.cs
+++ Experiments/Algorithm04Experiments.cs
@@ -19,8 +19,8 @@ namespace console.Experiments
             thread1.Start();
             Thread thread2 = new Thread(PerformAlg04param02);
             thread2.Start();
-            // Thread thread3 = new Thread(PerformAlg04param03);
-            // thread3.Start();
+            Thread thread3 = new Thread(PerformAlg04param03);
+            thread3.Start();
 
         }
 
@@ -137,27 +137,28 @@ namespace console.Experiments
 
         static void performAlg04(FuzzyTable table, int indexForParam)
         {
-            Console.WriteLine("performAlg04Exp");
+            Console.WriteLine("performAlg04Exp: "+indexForParam);
             int size = 10;
             Double[] kriteriaArray = new Double[size];
             for (int i = 0; i < size; i++)
             {
                 var beta = 0 + 0.1 * i;
                 var dataSize = 0;
-                for (int j = 0; j < 20; j++)
+                for (int j = 0; j < 50; j++)
                 {
-                    Algorithm04 alg02 = new Algorithm04(beta,  0.7, 0.9);;
-                    // switch(indexForParam){
-                    //     case 0:
-                    //     alg02 = new Algorithm04(beta,  0.7, 0.9);
-                    //     break; 
-                    //     case 1:
-                    //     alg02 = new Algorithm04(0,beta, 0.9);
-                    //     break;
-                    //     default:
-                    //     alg02 = new Algorithm04(0, 0, beta);
-                    //     break;
-                    // }
+                    // Algorithm04 alg02 = new Algorithm04(beta,  0.7, 0.9);
+                    Algorithm04 alg02;
+                    switch(indexForParam){
+                        case 0:
+                        alg02 = new Algorithm04(beta,  0, 0);
+                        break; 
+                        case 1:
+                        alg02 = new Algorithm04(0,beta, 0);
+                        break;
+                        default:
+                        alg02 = new Algorithm04(0, 0, beta);
+                        break;
+                    }
 
                     alg02.init(table);
                     var validation02 = new TenCrossValidation();
