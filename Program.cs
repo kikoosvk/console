b39a536a4c8193cd34506e788d3d1358a3f5e37e
Chudjak <chudjakk@gmail.com>
2019-11-24 16:37:59 +0100

    asdf

--- Program.cs
+++ Program.cs
@@ -16,14 +16,14 @@ namespace console
     {
         static void Main(string[] args)
         {
-            Algorithm01Experiments.run();
-            return;
+            // Algorithm01Experiments.run();
+            // return;
             var table = new FuzzyTable();
             try
             {   // Open the text file using a stream reader.
                 // using (StreamReader sr = new StreamReader("test.txt"))
                 // using (StreamReader sr = new StreamReader("./data/abalone/3bins/abalone_fuzzy.json"))
-                using (StreamReader sr = new StreamReader("./data/car/car.json"))
+                using (StreamReader sr = new StreamReader("./data/hepatitis/hcv_fuzzy.json"))
 
                 {
                     String json = sr.ReadToEnd();
@@ -35,7 +35,7 @@ namespace console
                     {
                         table.addAttribute(array.attributes[i]);
                     }
-                    table.addClassAttribute(array.attributes[array.attributes.Count - 1], "unacc", "acc");
+                    table.addClassAttribute(array.attributes[array.attributes.Count - 1], "high", "low");
                     // table.addClassAttribute(array.attributes[array.attributes.Count - 1], "small", "big");
 
                     table.AddData(array.data);
@@ -48,27 +48,27 @@ namespace console
                     // var validator = new TenCrossValidation();
                     // validator.Validate02(10, table);
                     var psi = new Dictionary<string, double>();
-                    psi["acc"] = 0.6;
-                    psi["unacc"] = 0.8;
+                    psi["low"] = 0.6;
+                    psi["high"] = 0.8;
                     // psi["old"] = 0.8;
 
-                        // performAlg04(table, 2);
-                    var alg01 = new Algorithm(0.1, 0.6);
-                    alg01.init(table);
-                    var validation01 = new TenCrossValidation();
-                    var matrix01 = validation01.Validate02(10, table, alg01);
+                    //     // performAlg04(table, 2);
+                    // var alg01 = new Algorithm(0.1, 0.6);
+                    // alg01.init(table);
+                    // var validation01 = new TenCrossValidation();
+                    // var matrix01 = validation01.Validate02(10, table, alg01);
 
-                    var alg02 = new Algorithm02(0.1, psi);
-                    alg02.init(table);
-                    var validation02 = new TenCrossValidation();
-                    var matrix02 = validation02.Validate02(10, table, alg02);
+                    // var alg02 = new Algorithm02(0.1, psi);
+                    // alg02.init(table);
+                    // var validation02 = new TenCrossValidation();
+                    // var matrix02 = validation02.Validate02(10, table, alg02);
 
-                    var alg03 = new Algorithm03(0.1, 0.6, 0.88);
-                    alg03.init(table);
-                    var validation03 = new TenCrossValidation();
-                    var matrix03 = validation03.Validate02(10, table, alg03);
+                    // var alg03 = new Algorithm03(0.1, 0.6, 0.88);
+                    // alg03.init(table);
+                    // var validation03 = new TenCrossValidation();
+                    // var matrix03 = validation03.Validate02(10, table, alg03);
 
-                    var alg04 = new Algorithm04(0.4, 0.6, 0.7);
+                    var alg04 = new Algorithm04(0.1, 0.7, 0.8);
                     alg04.init(table);
                     var validation04 = new TenCrossValidation();
                     var matrix04 = validation04.Validate02(10, table, alg04);
